11a1d238b03978c7bf005a8381b1fdc77e68cd17
Jason Cavaliere <Jason Cavaliere>
2016-07-13 12:48:44 -0500

    updated angular version for ES ui sample, fixed packages for webapi

--- src/Samples/EventStore/TechFu.Nirvana.EventStoreSample.UI/systemjs.config.js
+++ src/Samples/EventStore/TechFu.Nirvana.EventStoreSample.UI/systemjs.config.js
@@ -22,10 +22,13 @@
         'moment':                     { defaultExtension: 'js' },
         'ng2-bootstrap':       { defaultExtension: 'js' }
     };
+
+
     var ngPackageNames = [
         'common',
         'compiler',
         'core',
+        'forms',
         'http',
         'platform-browser',
         'platform-browser-dynamic',
@@ -33,21 +36,31 @@
         'router-deprecated',
         'upgrade',
     ];
+
     // Individual files (~300 requests):
     function packIndex(pkgName) {
         packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
     }
+
     // Bundled (~40 requests):
     function packUmd(pkgName) {
         packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
     }
+
     // Most environments should use UMD; some (Karma) need the individual index files
     var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+
     // Add package entries for angular packages
     ngPackageNames.forEach(setPackageConfig);
+
+    // No umd for router yet
+    packages['@angular/router'] = { main: 'index.js', defaultExtension: 'js' };
+
     var config = {
         map: map,
         packages: packages
     };
+
     System.config(config);
+
 })(this);
