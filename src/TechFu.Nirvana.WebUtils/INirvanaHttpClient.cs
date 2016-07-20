672bf42f8f36b0956ff3cb0d1df60adb1922d197
Jason Cavaliere <jasoncavaliere@hotmail.com>
2016-07-20 10:26:45 -0500

    tidy tidy....

---
+++ src/TechFu.Nirvana.WebUtils/INirvanaHttpClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TechFu.Nirvana.CQRS;
+
+namespace TechFu.Nirvana.WebUtils
+{
+    public interface INirvanaHttpClient
+    {
+        Task<HttpResponseMessage> Command<T>(string requestUri, Command<T> value);
+        Task<HttpResponseMessage> Query<T>(string requestUrl, Query<T> value);
+        Task<HttpResponseMessage> UiEvent<T>(string requestUrl, UiEvent<T> value);
+    }
+}
--- src/TechFu.Nirvana.WebUtils/NirvanaHttpClient.cs
+++ src/TechFu.Nirvana.WebUtils/NirvanaHttpClient.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 using TechFu.Nirvana.CQRS;
 
 namespace TechFu.Nirvana.WebUtils
 {
-    public interface INirvanaHttpClient
-    {
-        Task<HttpResponseMessage> Command<T>(string requestUri, Command<T> value);
-        Task<HttpResponseMessage> Query<T>(string requestUrl, Query<T> value);
-        Task<HttpResponseMessage> UiEvent<T>(string requestUrl, UiEvent<T> value);
-    }
-
     public class NirvanaHttpClient : INirvanaHttpClient
     {
         public async Task<HttpResponseMessage> Command<T>(string requestUri, Command<T> value)
