674c700bfe978bf1171a3f253c93d02bc5da4cb0
Jason Cavaliere <jasoncavaliere@hotmail.com>
2016-08-04 09:06:01 -0500

    sleep 500 MS instead of 100 to help preserve local resources ( need to look more into this)

--- src/TechFu.Nirvana.AzureQueues/Handlers/AzureQueueController.cs
+++ src/TechFu.Nirvana.AzureQueues/Handlers/AzureQueueController.cs
@@ -22,7 +22,7 @@ namespace TechFu.Nirvana.AzureQueues.Handlers
                     Name = GetQueueName(x.Key, q),
                     NumberOfConsumers = 1,
                     Status = QueueStatus.Stopped,
-                    SleepInMSBetweenTasks = 100
+                    SleepInMSBetweenTasks = 500
                 }).Cast<QueueReference>().ToArray());
         }
 
