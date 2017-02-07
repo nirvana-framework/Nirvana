8cdc2d97c6b5831d6dfcf2ae663c9ab4d5597050
Jason Cavaliere <jason@quarterspot.net>
2017-02-07 17:51:19 -0600

    Adding a test to check if appveyor is working corectly

---
+++ src/TechFu.Nirvana.TestFramework/BddTestBase.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoNSubstitute;
+using TechFu.Nirvana.CQRS;
+using TechFu.Nirvana.Mediation;
+
+namespace TechFu.Nirvana.TestFramework
+{
+    public abstract class BddTestBase<TSut, TInput, TResult> 
+    {
+        protected BddTestBase()
+        {
+            SetupBuildAndRun();
+        }
+
+        protected IFixture Depends;
+        protected TInput Input;
+        public bool RequiresIoCContainer = false;
+        protected TResult Result;
+        protected TSut Sut;
+
+        public abstract Action Inject { get; }
+        public virtual Action Establish => () => { };
+        public abstract Action Because { get; }
+
+        private void SetupBuildAndRun()
+        {
+            Depends = new Fixture().Customize(new AutoNSubstituteCustomization());
+            Inject();
+            Build();
+            Establish();
+
+            RunTest();
+        }
+
+        private void RunTest()
+        {
+            Because();
+        }
+
+        public void Build()
+        {
+            Sut = Depends.Create<TSut>();
+        }
+
+        public T DependsOn<T>()
+        {
+            return Depends.Freeze<T>();
+        }
+
+        public T CreateStub<T>()
+        {
+            return Depends.Create<T>();
+        }
+
+        public void DependsOnConcrete<T>(T input)
+        {
+            Depends.Inject(input);
+        }
+    }
+
+    public abstract class BddQueryTestBase<TSutType, TQueryType, TResponseType>
+        : BddTestBase<TSutType, TQueryType, QueryResponse<TResponseType>>
+        where TQueryType : Query<TResponseType>, new()
+        where TSutType : IQueryHandler<TQueryType, TResponseType>
+
+    {
+        public override Action Because => () => Result = Sut.Handle(Input);
+    }
+
+    public abstract class BddCommandTestBase<TSutType, TQueryType, TResponseType>
+        : BddTestBase<TSutType, TQueryType, CommandResponse<TResponseType>>
+        where TQueryType : Command<TResponseType>, new()
+        where TSutType : ICommandHandler<TQueryType, TResponseType>
+
+    {
+        public override Action Because => () => Result = Sut.Handle(Input);
+    }
+
+    
+}
---
+++ src/TechFu.Nirvana.Tests/Configuration/NirvanaSetupTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using Should;
+using TechFu.Nirvana.Configuration;
+using TechFu.Nirvana.TestFramework;
+using Xunit;
+
+namespace TechFu.Nirvana.Tests.Configuration
+{
+    public abstract class NirvanaSetupTests
+    {
+        public class when_setting_SetRootTypeAssembly :
+            BddTestBase<NirvanaConfigurationHelper, Assembly, object>
+        {
+            public override Action Inject => () => { };
+
+            public override Action Because
+                => () => { NirvanaSetup.Configure().SetRootTypeAssembly(typeof(Console).Assembly); };
+            
+            [Fact]
+            public void should_set_root_type_assembly()
+            {
+              NirvanaSetup.RootTypeAssembly.FullName.ShouldEqual(typeof(Console).Assembly.FullName);
+            }
+        }
+    }
+}
