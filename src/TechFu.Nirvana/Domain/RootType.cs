7ac035cc442aa028f4a86416e8ee372490137e45
Jason Cavaliere <jasoncavaliere@hotmail.com>
2017-02-14 08:59:10 -0600

    Added public / Private delineation in root type attribute

--- src/TechFu.Nirvana/Domain/RootType.cs
+++ src/TechFu.Nirvana/Domain/RootType.cs
@@ -13,6 +13,7 @@ namespace Nirvana.Domain
         public string RootName => RootType.RootName;
         public RootType RootType { get; private set; }
         public string Identifier { get; private set; }
+        public bool Public{ get; private set; }
 
         public static string FilterName(string cqrsTypeName )
         {
@@ -35,10 +36,11 @@ namespace Nirvana.Domain
         }
 
 
-        protected AggregateRootAttribute(RootType rootType, Type parameterType)
+        protected AggregateRootAttribute(RootType rootType, Type parameterType,bool isPublic=false)
         {
             RootType = rootType;
             Identifier = FilterName(parameterType.Name);
+            Public = isPublic;
         }
     }
 }
